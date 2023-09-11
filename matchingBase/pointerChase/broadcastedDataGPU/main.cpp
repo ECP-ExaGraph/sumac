54e6518e4f6f0c25bffbb33c67e1e2c0ecfd8827
Michael Mandulak <mandum@rpi.edu>
2023-09-11 04:24:17 -0400

    Initial version of nccl comm for bcast

---
+++ matchingBase/pointerChase/broadcastedDataGPU/main.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <omp.h>
+#include "graph.hpp"
+#include "graph_gpu.hpp"
+#include "types.hpp"
+#include "cuda_wrapper.hpp"
+
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+
+
+#define gpuErrchk(ans) {gpuAssert((ans), __FILE__,__LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true){
+	if (code != cudaSuccess)
+	{
+		fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort) exit(code);
+	}
+}
+
+
+
+
+int main(int argc, char** argv)
+{
+    Graph* graph = nullptr;
+    std::string inputFileName = "/home/mandum/PNNLwork/G33.bin";
+    //std::string inputFileName = "/data/graphs/U1a.bin";
+    graph = new Graph(inputFileName);
+
+    GraphGPU* graph_gpu = new GraphGPU(graph, 4, 1, 1);
+    cudaDeviceSynchronize();
+    
+    printf("Starting Matching\n");
+    double start; 
+    double end; 
+    start = omp_get_wtime(); 
+    graph_gpu->run_pointer_chase();
+    cudaDeviceSynchronize();
+    //gpuErrchk( cudaPeekAtLastError() );
+    //gpuErrchk( cudaDeviceSynchronize() );
+
+    end = omp_get_wtime(); 
+
+    printf("Finished Matching\n");
+    printf("Time Elapsed: %f seconds\n", end - start);
+
+    
+    return 0;
+}
